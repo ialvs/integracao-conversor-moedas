b61a555cf2e4a4c4f620d53f8247f5630d168c66
Ian Alves <ian.santos.alves@hotmail.com>
2023-02-26 22:58:06 -0300

    Exemplo de apivalue.ts adicionado como comentário

--- api-converter-moedas/src/utils/apikey.ts
+++ api-converter-moedas/src/utils/apikey.ts
@@ -1,5 +1,12 @@
 import { apivalue } from './apivalue';
 
+/*
+export const apivalue = {
+  keyvalue: 'chaveDaAPITerceira',
+};
+
+*/
+
 export const apikey = {
   id: 'apikey',
   keyvalue: apivalue.keyvalue,
