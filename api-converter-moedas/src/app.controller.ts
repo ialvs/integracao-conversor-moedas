015a6839b2f02cc191c5c076965e9f56fabb2bce
Ian Alves <ian.santos.alves@hotmail.com>
2023-02-26 22:14:00 -0300

    Adicionado redirecionamento do root para GET /users

--- api-converter-moedas/src/app.controller.ts
+++ api-converter-moedas/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Redirect('http://localhost:3000/api/v1/users')
+  getRedirect(): string {
+    return this.appService.redirect();
   }
 }
